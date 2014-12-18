9780806a447163e47cc48d76cbe37d1041d58c93
sprc86 <sprc86@gmail.com>
2014-12-18 13:31:48 -0200

    Committer: sprc86 <sprc86@gmail.com>
    	new file:   kill_sleepuser_mysql.sh

---
+++ kill_sleepuser_mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+# Get the processlist and save it in a file
+mysqladmin -u root -p"admin_password" processlist > allprocesses
+
+
+#lists all the processes for the sleeping user and send the output to the "id" file
+cat allprocesses | awk '{if ($4 == "insert_user_here" && $10 == "Sleep") print $2;}' > id
+
+
+
+for sleep_id in `cat ./id`
+do
+  # Kill each id identified earlier.
+  mysqladmin -u root -p"yourpassword" KILL $sleep_id ;
+done
+
+# Remove files
+rm  allprocesses
+rm id
