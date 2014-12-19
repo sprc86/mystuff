73cafd7499419505d14598f992b248023050f8f5
sprc86 <sprc86@gmail.com>
2014-12-19 11:50:37 -0200

    included temporary files to /tmp/

--- kill_sleepuser_mysql.sh
+++ kill_sleepuser_mysql.sh
@@ -3,20 +3,20 @@
 
 
 # Get the processlist and save it in a file
-mysqladmin -u root -p"admin_password" processlist > allprocesses
+mysqladmin -u root -p"admin_password" processlist > /tmp/allprocesses
 
 
 #lists all the processes for the sleeping user and send the output to the "id" file
-cat allprocesses | awk '{if ($4 == "insert_user_here" && $10 == "Sleep") print $2;}' > id
+cat /tmp/allprocesses | awk '{if ($4 == "insert_user_here" && $10 == "Sleep") print $2;}' > /tmp/id
 
 
 
-for sleep_id in `cat ./id`
+for sleep_id in `cat /tmp/id`
 do
   # Kill each id identified earlier.
   mysqladmin -u root -p"admin_password" KILL $sleep_id ;
 done
 
 # Remove files
-rm  allprocesses
-rm id
+rm  /tmp/allprocesses
+rm /tmp/id
