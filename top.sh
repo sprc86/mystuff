2b07269992c740bcc9b3588fe0d8c15c680b0d86
Rodrigo de Campos <sprc86@gmail.com>
2018-12-05 23:50:52 +0000

    fixed sed filter

--- top.sh
+++ top.sh
@@ -8,6 +8,6 @@ do
     date
     echo "Binary:  `ls -lah /proc/$pid/exe | cut -d' ' -f11`"
     echo "Owner:  `ls -lah /proc/$pid/exe | cut -d' ' -f3`"
-    echo "Memory usage in kB:  `cat /proc/$pid/status | grep VmSize | cut -d' ' -f3`"
+    echo "Memory usage in kB:  `cat /proc/$pid/status | grep VmSize | cut -d':' -f2`"
     sleep 5
 done
